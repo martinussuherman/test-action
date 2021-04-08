260aa93ec3ec85166c9e426e684090e9db5528c4
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-08 23:49:02 +0700

    Simplify script

--- glibc-build/builder.sh
+++ glibc-build/builder.sh
@@ -1,27 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -eo pipefail; [[ "$TRACE" ]] && set -x
+set -eo pipefail
+[[ "$TRACE" ]] && set -x
+
+version=$GLIBC_VERSION
+prefix=$PREFIX_DIR
 
 main() {
-	declare version="${1:-$GLIBC_VERSION}" prefix="${2:-$PREFIX_DIR}"
-
-	: "${version:?}" "${prefix:?}"
-
-	{
-		wget -qO- "https://ftpmirror.gnu.org/libc/glibc-$version.tar.gz" \
-			| tar zxf -
-		mkdir -p /glibc-build && cd /glibc-build
-		"/glibc-$version/configure" \
-			--prefix="$prefix" \
-			--libdir="$prefix/lib" \
-			--libexecdir="$prefix/lib" \
-			--enable-multi-arch \
-			--enable-stack-protector=strong
-		make && make install
-		tar --dereference --hard-dereference -zcf "/glibc-bin-$version.tar.gz" "$prefix"
-	} >&2
-
-	[[ $STDOUT ]] && cat "/glibc-bin-$version.tar.gz"
+    wget -qO- "https://ftpmirror.gnu.org/libc/glibc-$version.tar.gz" | tar zxf -
+    mkdir -p /glibc-build /glibc-tar && cd /glibc-build
+    "/glibc-$version/configure" --prefix="$prefix" --libdir="$prefix/lib" --libexecdir="$prefix/lib" --enable-multi-arch --enable-stack-protector=strong
+    # "../glibc-$version/configure" --prefix="$prefix" --libdir="$prefix/lib" --libexecdir="$prefix/lib" --enable-stack-protector=strong
+    make -j 4 && make install
+    tar --dereference --hard-dereference -zcf "/glibc-tar/glibc-bin-$version.tar.gz" "$prefix"
 }
 
 main "$@"
