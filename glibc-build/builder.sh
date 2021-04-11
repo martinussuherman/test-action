d63fd7700d46f5a6127a95f843039340b6def794
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-11 23:40:29 +0700

    Remove extra line

--- glibc-build/builder.sh
+++ glibc-build/builder.sh
@@ -10,7 +10,6 @@ main() {
     wget -qO- "https://ftpmirror.gnu.org/libc/glibc-$version.tar.gz" | tar zxf -
     mkdir -p /glibc-build /glibc-tar && cd /glibc-build
     "/glibc-$version/configure" --prefix="$prefix" --libdir="$prefix/lib" --libexecdir="$prefix/lib" --enable-multi-arch --enable-stack-protector=strong
-    # "../glibc-$version/configure" --prefix="$prefix" --libdir="$prefix/lib" --libexecdir="$prefix/lib" --enable-stack-protector=strong
     make -j 4 && make install
     tar --dereference --hard-dereference -zcf "/glibc-tar/glibc-bin-$version.tar.gz" "$prefix"
 }
